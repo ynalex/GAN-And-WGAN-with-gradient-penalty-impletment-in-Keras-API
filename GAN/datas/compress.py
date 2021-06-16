b53e442b81fdab8d2387c79cdb2561f293e3fc31
ynalex <73702697+ynalex@users.noreply.github.com>
2021-06-16 15:06:27 +0800

    Add files via upload

---
+++ GAN/datas/compress.py
@@ -0,0 +1,37 @@
+import numpy as np
+import cv2
+import os
+import random
+
+def compress_image(photo_array, mutiplier):
+    photo_array = np.array(photo_array)
+
+    h = int(photo_array.shape[0])
+    w = int(photo_array.shape[1])
+    new_h = int(h/mutiplier)
+    new_w = int(w/mutiplier)
+
+    print("Compressing images with size".format(size) + " ({},".format(h) + "{}".format(w) + ").")
+    
+    new_image = np.zeros([new_h, new_w, 3])
+
+    for i in range(int(h/mutiplier)):
+        for j in range(int(w/mutiplier)):
+                for z in range(3):
+                    new_image[int(i),int(j),z] = photo_array[i*mutiplier,j*mutiplier,z]
+
+    return new_image
+
+img_folder_path = 'CelebAMask-HQ/CelebA-HQ-img'
+size = 10000
+os.makedirs('Image', exist_ok=True)
+save_path = os.path.join(img_folder_path, 'Image')
+
+for file in sorted(os.listdir(img_folder_path)):
+        image_path = os.path.join(img_folder_path, file)
+        image = cv2.imread(image_path,cv2.COLOR_RGB2BGR)
+        image = np.array(image)
+        image = compress_image(image,4)
+        save_name = os.path.join('Image', file)
+        print("Saving {}.".format(file))
+        cv2.imwrite(save_name, image)
